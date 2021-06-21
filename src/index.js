948650cc60d149f1ca066e07b91c14dc99db3866
Abdullah al Sakib <abdullahalsakib903@gmail.com>
2021-06-21 21:03:46 +0600

    fixed some css issue --> removed normalize.css 😐

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
-import "./Normalize.css";
 
 ReactDOM.render(
   <BrowserRouter>
